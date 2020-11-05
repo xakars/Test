f8cebd8b3a0ce6ebb39f900234c35165fd44ecad
Arsen Hakimov <xakars02@gmail.com>
2020-11-05 18:29:57 +0300

    new folder for Linux

---
+++ Linux/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pwd
+cd Documents
+mkdir folder1 folder2 folder3
+cd folder1
+touch QAtxt1.txt QAtxt2.txt QAtxt3.txt QAjson1.json QAjson2.json
+mkdir folder_1 folder_2 folder_3
+ls
+mv QAjson1.json QAjson2.json ~/Documents/folder2
+
